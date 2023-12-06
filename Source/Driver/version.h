154f27ed423a792b3371621bd4b75af7d866b375
zbacsko <zbacsko@dexter.hu>
2023-12-06 09:28:48 +0100

    Fixed some missing checkings

--- Source/Driver/version.h
+++ Source/Driver/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
 #define VERSION_PATCH 1
-#define VERSION_BUILD 1
+#define VERSION_BUILD 2
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
